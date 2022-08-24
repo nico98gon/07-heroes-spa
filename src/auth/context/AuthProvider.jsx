bc42f78d23121c792eb9f8b64a6d0afcc3a5c0ce
Nicolas Gonzalez <nicoaugustogonzalez14@gmail.com>
2022-08-24 17:31:38 -0300

    209.

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,37 @@
+import { useReducer } from "react"
+import { AuthContext } from "./AuthContext"
+import { authReducer } from "./AuthReducer";
+
+import { types } from "../types/types";
+
+const initialState = {
+    logged: false,
+    user: user,
+}
+
+export const AuthProvider = ({ children }) => {
+
+    const [ authState, dispatch ] = useReducer( authReducer, initialState );
+
+    const login = ( name = '' ) => {
+        
+        const user = {
+            id: 'ABC',
+            name
+        }
+        const action = {
+            type: types.login,
+            payload: user,
+        }
+        dispatch(action);
+    }
+
+    return (
+        <AuthContext.Provider value={{
+            ...authState,
+            login: login
+        }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
